e07c5731d821d7386865c39c2f3dcd16021514bd
Bruxo <bruxo@cock.li>
2026-07-10 22:31:54 +0330

    this is the best version I've made.

--- app/bot/handlers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .start import get_handler
-
-__all__ = (
-    "get_handler",
-)
--- app/bot/router.py
+++
@@ -1,7 +0,0 @@
-from telegram.ext import Application
-
-from app.bot.handlers.start import start
-
-
-def register_handlers(application: Application) -> None:
-    application.add_handler(start())
